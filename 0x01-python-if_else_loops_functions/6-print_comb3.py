68f59762d1555c83bf497e281a622d4d8c556034
brikhotso <brenda.rikhotso@gmail.com>
2023-11-28 12:32:59 -0800

    requirement check

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for box in range(10):
     for toy in range(box + 1, 10):
-        print(f"{box}{toy}", end=', ' if (box != 8 or toy != 9) else '')
-print()
+        if  box != 8 or toy != 9:
+            print(f"{box}{toy}", end=", ")
+        else:
+            print(f"{box}{toy}")
