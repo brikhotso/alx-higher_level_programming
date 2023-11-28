ff0e2fe0b982024a83aac5d70b12a3149cc90e12
brikhotso <brenda.rikhotso@gmail.com>
2023-11-28 12:14:04 -0800

    corrections

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-
-    if b < 0:
-        a = 1 / a
-        b = abs(b)
-
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
+    return (a ** b)
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if 'a' <= c <= 'z':
+    if c and 'a' <= c <= 'z':
         return True
     return False
