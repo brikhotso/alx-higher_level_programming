80d0f421d1108e40e505a2722677995d0718f419
brikhotso <brenda.rikhotso@gmail.com>
2023-11-28 13:44:56 -0800

    fix if ststement

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,5 @@
 for i in range(26):
     if i % 2 == 0:
         print("{:c}".format(122 - i), end='')
-    print("{:c}".format(90 - i), end='')
+    else:
+        print("{:c}".format(90 - i), end='')
