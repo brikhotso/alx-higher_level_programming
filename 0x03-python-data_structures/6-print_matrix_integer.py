90b0ed77de2f650059a7ba0ae9bfdb3f9265c924
brikhotso <brenda.rikhotso@gmail.com>
2023-12-02 07:46:52 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end=' ')
-
+            if element == row[-1]:
+                print("{:d}".format(element), end='')
+            else:
+                print("{:d}".format(element), end=' ')
         print()
