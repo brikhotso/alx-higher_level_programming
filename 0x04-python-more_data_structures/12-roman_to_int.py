b355019eed826be69c8f64d55a396a9660596228
brikhotso <brenda.rikhotso@gmail.com>
2023-12-06 10:29:51 -0800

    convert roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return (0)
+
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = my_dict.get(char, 0)
+
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+
+        prev_value = value
+
+    return (total)
