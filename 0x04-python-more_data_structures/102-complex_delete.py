b0c86962631d331af662791b12a485e7a76a1e57
brikhotso <brenda.rikhotso@gmail.com>
2023-12-06 11:20:22 -0800

    weighted average, delete , square matrix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/python3
 def complex_delete(a_dictionary, value):
     for k in list(a_dictionary.keys()):
-        if a_dictionary[k] is value:
+        if a_dictionary[k] == value:
             del a_dictionary[k]
-    return (a_dictionary)
+    return a_dictionary
