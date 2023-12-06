7085be6cabd4c0ea3856411dda70f11749998f2b
brikhotso <brenda.rikhotso@gmail.com>
2023-12-06 05:20:42 -0800

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/phython3
 
 def search_replace(my_list, search, replace):
-    orig_list = my_list.copy()
-
-    for i in range(len(orig_list)):
-        if orig_list[i] == search:
-            orig_list[i] = replace
-
-    return orig_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return new_list
