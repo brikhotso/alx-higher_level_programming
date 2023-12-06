f8b099583c6ee6719e61998c91250f6ff5fc77ee
brikhotso <brenda.rikhotso@gmail.com>
2023-12-06 12:44:26 -0800

    python lists and objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -7,38 +7,23 @@
  */
 void print_python_list(PyObject *p)
 {
-	long int size = 0, i = 0;
-	PyObject *item;
+	long int size, i;
+	PyListObject *list;
+	PyObject *obj;
 
-	if (!PyList_Check(p))
-	{
-		fprintf(stderr, "Invalid PyListObject\n");
-		return;
-	}
-
-	while (1)
-	{
-		item = PyList_GetItem(p, size);
-		if (item == NULL)
-			break;
-		size++;
-	}
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (i = 0; i < size; ++i)
+	for (i = 0; i < size; i++)
 	{
-		if (item != NULL)
-		{
-			printf("[*] Element %ld: %s\n", i,
-			       item->ob_type->tp_name);
-		}
-		else
-		{
-			printf("[*] Element %ld: Unknown\n", i);
-		}
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
 	}
 }
 
