d601f9e6dd8fb30e603590c9606a5af6e5459004
brikhotso <brenda.rikhotso@gmail.com>
2023-12-06 11:51:10 -0800

    pycodestyle

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
-#!/usr/python3
+#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k in list(a_dictionary.keys()):
+    key_list = list(a_dictionary.keys())
+
+    for k in key_list:
         if value == a_dictionary.get(k):
             del a_dictionary[k]
 
