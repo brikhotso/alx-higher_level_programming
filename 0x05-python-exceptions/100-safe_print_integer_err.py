401100b9a7967fe3d9c935b1f8bb23d28ddf7f54
brikhotso <brenda.rikhotso@gmail.com>
2023-12-18 11:30:05 -0800

    print stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/urs/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
+        return False
+    return(value)
