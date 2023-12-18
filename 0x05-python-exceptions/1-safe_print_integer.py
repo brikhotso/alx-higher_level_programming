72f449a6c52860ffb6db4d3746e05e0e9189e3b9
brikhotso <brenda.rikhotso@gmail.com>
2023-12-18 05:11:00 -0800

    handle typeerror

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         pass
     return False
