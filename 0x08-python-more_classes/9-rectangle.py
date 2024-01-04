d3998dafc19b65aa321a39d4659c7cdf85e31683
brikhotso <brenda.rikhotso@gmail.com>
2024-01-04 09:14:48 -0800

    typo on error message

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -33,11 +33,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle."""
+        """Set width of the rectangle."""
         if not isinstance(value, int):
-            raise TypeError("Width must be an integer.")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("Width must be >= 0.")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -49,9 +49,9 @@ class Rectangle:
     def height(self, value):
         """Set the height of the rectangle."""
         if not isinstance(value, int):
-            raise TypeError("Height must be an integer.")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("Height must be >= 0.")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
