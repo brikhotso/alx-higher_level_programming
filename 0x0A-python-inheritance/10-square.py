a5ae3885b02f4196f1c6f39cee064eba4aa64dd1
brikhotso <brenda.rikhotso@gmail.com>
2024-01-08 12:16:05 -0800

    class square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialize a Square instance."""
+        self.integer_validation("size",size)
+        self.__size = size
         super().__init__(size, size)
 
-    def __str__(self):
-        """Return the string representation of the square."""
-        return "[Square] {}/{}".format(self._Rectangle__width,
-                                       self._Rectangle__height)
+    def area(self):
+        """ Return area of a square"""
+        return self.__size * 2
