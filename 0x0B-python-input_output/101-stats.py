98873989d92cc464f31cc6b7def081cb4a63a0bc
brikhotso <brenda.rikhotso@gmail.com>
2024-01-09 12:31:54 -0800

    stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,26 +14,30 @@ def print_status(lines):
     size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
-    for line in lines:
-        parts = line.split()
-        try:
-            size += int(parts[-1])
-            code = parts[-2]
-            status_codes[code] += 1
-        except(ValueError, IndexError):
-            continue
-        if counter == 9:
+    try:
+        for line in lines:
+            parts = line.split()
+            try:
+                size += int(parts[-1])
+                code = parts[-2]
+                status_codes[code] += 1
+            except(ValueError, IndexError):
+                continue
+            if counter == 9:
+                print("File size: {}".format(size))
+                for key, val in sorted(status_codes.items()):
+                    if val:
+                        print("{}: {}".format(key, val))
+                counter = 0
+            counter += 1
+        if counter < 9:
             print("File size: {}".format(size))
             for key, val in sorted(status_codes.items()):
                 if (val != 0):
                     print("{}: {}".format(key, val))
-            counter = 0
-        counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
-        for key, val in sorted(status_codes.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
+
+    except KeyboardInterrupt:
+        print_stats(lines)
 
 
 if __name__ == "__main__":
