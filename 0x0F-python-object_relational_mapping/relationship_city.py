1aa6ef2dd986d7db5cda2ca6e940211e2d5db8ef
brikhotso <brenda.rikhotso@gmail.com>
2024-03-16 08:23:04 -0700

    create state with city sqlalchemy

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -19,6 +19,6 @@ class City(Base):
 
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
