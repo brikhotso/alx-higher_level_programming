b39f6f298f8f920d14bb51b9d68dd74c0c7c380d
brikhotso <brenda.rikhotso@gmail.com>
2024-03-16 04:33:43 -0700

    class atrribute id unique integer

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,7 +15,10 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ Describe clase State Base"""
+
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
