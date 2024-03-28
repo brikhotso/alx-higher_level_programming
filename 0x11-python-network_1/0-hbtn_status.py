10e08a3170fb930eb802d35a5789ebb35aa0b2db
brikhotso <brenda.rikhotso@gmail.com>
2024-03-28 04:56:18 -0700

    format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read().decode('utf-8')
 
 print("Body response:")
-print("\t- type:", type(body))
+print("\t- type: {}".format(type(body)))
 print("\t- content: {}".format(body))
 print("\t- utf8 content: {}".format(body))
