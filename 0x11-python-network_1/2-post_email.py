bf252453920d0b465d873d8986027fca3e3f3317
brikhotso <brenda.rikhotso@gmail.com>
2024-03-28 05:33:08 -0700

    code should not be executed when imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,13 @@ import urllib.request
 import sys
 
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({"email": email}).encode('utf-8')
-request = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode({"email": email}).encode('utf-8')
+    request = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(request) as response:
-    body = response.read().decode('utf-8')
-
-print(body)
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode('utf-8')
+        print(body)
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,12 +4,10 @@ import sys
 import urllib.error
 import urllib.request
 
-
-url = sys.argv[1]
-
-request = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("ascii"))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    request = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
