5aa3e2d6ec3017ebc1e26c5c2587fd1fbe60e7c7
brikhotso <brenda.rikhotso@gmail.com>
2024-03-28 05:27:27 -0700

    code should not be executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@
 import urllib.request
 import sys
 
-
-request = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(request) as response:
-    print(dict(response.headers).get("X-Request-Id"))
+if __name__ == "__main__":
+    request = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
